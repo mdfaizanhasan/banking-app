04b8742838f5ca454842c22d66f7ac9ac70e0cb9
faizanhasan <mdfaizanalam367@gmail.com>
2024-06-26 22:26:21 +0530

    Implement initial file and folder structure

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,15 @@
+
+export default async function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+
+  return (
+    <main className="flex h-screen w-full font-inter">
+      <div className="flex size-full flex-col">
+        {children}
+      </div>
+    </main>
+  );
+}
