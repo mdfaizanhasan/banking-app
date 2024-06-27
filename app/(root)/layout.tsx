593ead94c029c09af18f03675020c08864201684
faizanhasan <mdfaizanalam367@gmail.com>
2024-06-27 21:10:38 +0530

    Implements the complete Sidebar and layouts

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,13 +1,30 @@
 
+// import { getLoggedInUser } from "@/lib/actions/user.actions";
+import MobileNav from "@/components/MobileNav";
+import Sidebar from "@/components/Sidebar";
+import Image from "next/image";
+// import { redirect } from "next/navigation";
+
 export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const loggedIn = {firstName: "Faizan", lastName: "Alam"};
+
+  // if(!loggedIn) redirect('/sign-in')
 
   return (
     <main className="flex h-screen w-full font-inter">
+      <Sidebar user={loggedIn} />
+
       <div className="flex size-full flex-col">
+        <div className="root-layout">
+          <Image src="/icons/logo.svg" width={30} height={30} alt="logo" />
+          <div>
+            <MobileNav user={loggedIn} />
+          </div>
+        </div>
         {children}
       </div>
     </main>
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,39 @@
-import React from 'react'
+import HeaderBox from "@/components/HeaderBox";
+import RightSidebar from "@/components/RightSidebar";
+import TotalBalanceBox from '@/components/TotalBalanceBox';
 
-const Home = () => {
+
+const Home = async ({ searchParams: { id, page } }: SearchParamProps) => {
+  const currentPage = Number(page as string) || 1;
+  const loggedIn = {firstName: 'Faizan Hasan', email: "mdfaizanhasan367@gmail.com"};
+
   return (
-    <div>Home</div>
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox 
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firstName || 'Guest'}
+            subtext="Access and manage your account and transactions efficiently."
+          />
+
+          <TotalBalanceBox 
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={1250.51}
+          />
+        </header>
+
+        Recent Transactions
+      </div>
+
+      <RightSidebar 
+        user={loggedIn}
+        transactions={[]}
+        banks={[{currentBalance: 125.50}, {currentBalance: 156.70}]}
+      />
+    </section>
   )
 }
 
