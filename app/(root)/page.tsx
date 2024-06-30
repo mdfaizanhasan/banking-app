8b892359fe678201a6f0528db0fcf4656fb71dab
faizanhasan <mdfaizanalam367@gmail.com>
2024-06-30 16:04:28 +0530

    Implement the Plaid Link and Dwolla

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,22 +1,22 @@
 import HeaderBox from '@/components/HeaderBox'
 import RightSidebar from '@/components/RightSidebar';
 import TotalBalanceBox from '@/components/TotalBalanceBox';
-// import { getAccount, getAccounts } from '@/lib/actions/bank.actions';
+import { getAccount, getAccounts } from '@/lib/actions/bank.actions';
 import { getLoggedInUser } from '@/lib/actions/user.actions';
 
 const Home = async ({ searchParams: { id, page } }: SearchParamProps) => {
   const currentPage = Number(page as string) || 1;
   const loggedIn = await getLoggedInUser();
-  // const accounts = await getAccounts({ 
-  //   userId: loggedIn.$id 
-  // })
+  const accounts = await getAccounts({ 
+    userId: loggedIn.$id 
+  })
 
-  // if(!accounts) return;
+  if(!accounts) return;
   
-  // const accountsData = accounts?.data;
-  // const appwriteItemId = (id as string) || accountsData[0]?.appwriteItemId;
+  const accountsData = accounts?.data;
+  const appwriteItemId = (id as string) || accountsData[0]?.appwriteItemId;
 
-  // const account = await getAccount({ appwriteItemId })
+  const account = await getAccount({ appwriteItemId })
 
   return (
     <section className="home">
@@ -30,18 +30,24 @@ const Home = async ({ searchParams: { id, page } }: SearchParamProps) => {
           />
 
           <TotalBalanceBox 
-            accounts={[]}
-            totalBanks={1}
-            totalCurrentBalance={1250.51}
+            accounts={accountsData}
+            totalBanks={accounts?.totalBanks}
+            totalCurrentBalance={accounts?.totalCurrentBalance}
           />
         </header>
-        Recent Transactions
+
+        {/* <RecentTransactions 
+          accounts={accountsData}
+          transactions={account?.transactions}
+          appwriteItemId={appwriteItemId}
+          page={currentPage}
+        /> */}
       </div>
 
       <RightSidebar 
         user={loggedIn}
-        transactions={[]}
-        banks={[{currentBalance: 125.50}, {currentBalance: 156.70}]}
+        transactions={account?.transactions}
+        banks={accountsData?.slice(0, 2)}
       />
     </section>
   )
